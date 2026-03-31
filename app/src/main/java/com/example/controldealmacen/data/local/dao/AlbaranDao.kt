a20494c98f559ebcd5bdeb1289235fe0f68d2c1f
kaarim17 <karim.sadikilo@elorrieta-errekamari.com>
2026-03-31 11:13:02 +0200

    sprint 3 y 4 terminados

--- app/src/main/java/com/example/controldealmacen/data/local/dao/AlbaranDao.kt
+++ app/src/main/java/com/example/controldealmacen/data/local/dao/AlbaranDao.kt
@@ -16,22 +16,22 @@ interface AlbaranDao {
     @Update
     suspend fun update(albaran: AlbaranEntity)
 
-    @Query("SELECT * FROM albaranes")
+    @Query("SELECT * FROM albaranes ORDER BY fecha DESC")
     suspend fun getAlbaranes(): List<AlbaranEntity>
 
     @Transaction
-    @Query("SELECT * FROM albaranes")
+    @Query("SELECT * FROM albaranes ORDER BY fecha DESC")
     suspend fun getAlbaranesConProveedor(): List<AlbaranConProveedor>
 
     @Transaction
-    @Query("SELECT * FROM albaranes WHERE id IN (:ids)")
+    @Query("SELECT * FROM albaranes WHERE id IN (:ids) ORDER BY fecha DESC")
     suspend fun getAlbaranesConProveedorByIds(ids: List<Int>): List<AlbaranConProveedor>
 
     // Filtra para mostrar en rojo los que no se han pagado
-    @Query("SELECT * FROM albaranes WHERE pagado = 0")
+    @Query("SELECT * FROM albaranes WHERE pagado = 0 ORDER BY fecha DESC")
     suspend fun getAlbaranesPendientes(): List<AlbaranEntity>
 
     // Busca todos los albaranes asociados a un proveedor usando el foreingKey
-    @Query("SELECT * FROM albaranes WHERE proveedorId = :idProveedor")
+    @Query("SELECT * FROM albaranes WHERE proveedorId = :idProveedor ORDER BY fecha DESC")
     suspend fun getAlbaranesbyProveedor(idProveedor: Int): List<AlbaranEntity>
 }
