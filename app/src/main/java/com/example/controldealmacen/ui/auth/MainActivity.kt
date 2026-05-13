bd935b2a848df455cf2ab56370bad75fd3bb6891
lukaulu <jonathan.mambuma@elorrieta-errekamari.com>
2026-05-13 11:49:15 +0200

    Conflicto solucionado

--- app/src/main/java/com/example/controldealmacen/ui/auth/MainActivity.kt
+++ app/src/main/java/com/example/controldealmacen/ui/auth/MainActivity.kt
@@ -26,12 +26,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-<<<<<<< HEAD
-=======
         // Al volver al estado de reposo, limpiamos cualquier sesión previa
         SessionManager.clearSession()
 
->>>>>>> 073f78b984c6dd6157c0a1fe9d34e89bbdd38e5b
         val rvEmpleados = findViewById<RecyclerView>(R.id.rv_empleados)
         rvEmpleados.layoutManager = GridLayoutManager(this, 4)
 
@@ -54,14 +51,8 @@ class MainActivity : AppCompatActivity() {
             adapter.actualizarDatos(listaPerfiles)
         }
 
-<<<<<<< HEAD
-        val btnNuevoEmpleado = findViewById<android.widget.Button>(R.id.btn_nuevo_empleado)
-        btnNuevoEmpleado.setOnClickListener {
-            val intent = android.content.Intent(this, RegistroActivity::class.java)
-=======
         findViewById<android.widget.Button>(R.id.btn_nuevo_empleado).setOnClickListener {
             val intent = Intent(this, RegistroActivity::class.java)
->>>>>>> 073f78b984c6dd6157c0a1fe9d34e89bbdd38e5b
             startActivity(intent)
         }
     }
@@ -75,14 +66,8 @@ class MainActivity : AppCompatActivity() {
         input.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
         builder.setView(input)
 
-<<<<<<< HEAD
-        builder.setPositiveButton("Entrar") { dialog, _ ->
-            val passwordIntroducida = input.text.toString()
-
-=======
         builder.setPositiveButton("Entrar") { _, _ ->
             val passwordIntroducida = input.text.toString()
->>>>>>> 073f78b984c6dd6157c0a1fe9d34e89bbdd38e5b
             if (passwordIntroducida == perfil.contrasena) {
                 SessionManager.isAdminAuthenticated = true
                 iniciarSesion(perfil)
@@ -99,6 +84,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun iniciarSesion(perfil: PerfilEntity) {
+        // Guardamos todo en el SessionManager global
         SessionManager.currentUserId = perfil.id
         SessionManager.currentUserRol = perfil.rol
         SessionManager.currentUserName = perfil.nombre
@@ -107,15 +93,7 @@ class MainActivity : AppCompatActivity() {
 
         val intent = Intent(this, ProductosActivity::class.java)
         intent.putExtra("ID_USUARIO", perfil.id)
-<<<<<<< HEAD
-        intent.putExtra("ROL_USUARIO", perfil.rol)
-        intent.putExtra("NOMBRE_USUARIO", perfil.nombre)
-
         startActivity(intent)
-
-=======
-        startActivity(intent)
->>>>>>> 073f78b984c6dd6157c0a1fe9d34e89bbdd38e5b
         finish()
     }
 }
